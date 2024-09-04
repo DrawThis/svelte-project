a3105285605d59659fca569c24593a11c60d39d0
Draw This <drawthis6@gmail.com>
2024-09-04 13:56:35 -0500

    Inicio

---
+++ mi-nuevo-proyecto/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import MiComponente from "./MiComponente.svelte";
+</script>
+
+<h1>Mi titulo del proyecto</h1>
+
+<h2>
+    Hablando del país:
+</h2>
+
+<p>Los paises que se mencionan son:</p>
+
+<MiComponente pais = Colombia capital = Bogotá ciudad = Medellin />
+
+<style>
+    * {
+        text-align: center;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    h1 {
+        color: red;
+        font-weight: 700;     
+    }
+
+</style>
---
+++ mi-nuevo-proyecto/src/routes/MiComponente.svelte
@@ -0,0 +1,18 @@
+<script>
+    let {pais, capital, ciudad} = $props()
+</script>
+
+<p>
+   una capital de <span>{pais}</span> es <span>{capital}</span> y una ciudad es <span>{ciudad}</span>
+</p>
+
+<style>
+    * {
+        text-align: center;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    span {
+        color: blue;
+    }
+</style>
